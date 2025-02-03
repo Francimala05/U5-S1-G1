77ac16f36980451e8039145630bba22050c00ffe
Francimala05 <francimalafronte05@gmail.com>
2025-02-03 22:40:39 +0100

    Esercizio completo

--- src/test/java/com/epicode/U5D1/Esercizio30102023ApplicationTests.java
+++ src/test/java/com/epicode/U5D1/Esercizio30102023ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.epicode.U5D1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBaseApplicationTests {
+class Esercizio30102023ApplicationTests {
 
 	@Test
 	void contextLoads() {
